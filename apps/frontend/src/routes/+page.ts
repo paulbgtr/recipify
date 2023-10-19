bde15cb0c7a56bad19f7aa0b0a6e73f2327edeb0
paulbg <paulbg@hey.com>
2023-10-19 15:33:33 +0300

    feat(routes): add new route for fetching random recipes from Spoonacular API
    This commit adds a new file `+page.ts` under `apps/frontend/src/routes` directory. The file contains a function `handleFetchRandomRecipes` that makes a GET request to the Spoonacular API to fetch 10 random recipes. The fetched recipes are then returned as a response.
    
    Additionally, the `load` function is exported from the file. This function is responsible for fetching the random recipes using `handleFetchRandomRecipes` and returning them as props.
    
    This new route will be used to display random recipes on a page in the frontend application.

---
+++ apps/frontend/src/routes/+page.ts
@@ -0,0 +1,32 @@
+import { PUBLIC_SPOONACULAR_API_KEY } from "$env/static/public";
+
+const handleFetchRandomRecipes = async () => {
+  const url = `https://api.spoonacular.com/recipes/random?number=10&apiKey=${PUBLIC_SPOONACULAR_API_KEY}`;
+
+  try {
+    const res = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await res.json();
+
+    const recipes = data.recipes;
+    return recipes;
+  } catch (err) {
+    return err;
+  }
+};
+
+/** @type {import('./$types').PageLoad} */
+export const load = async () => {
+  const randomRecipes = await handleFetchRandomRecipes();
+
+  return {
+    props: {
+      randomRecipes,
+    },
+  };
+};
