6923efda6797a73e660818afbd0d38f736656514
paulbg <paulbg@hey.com>
2023-10-19 08:01:20 +0300

    feat(Card.svelte): add Card component to display an image, title, description, and link
    The Card component is added to the frontend application. It accepts the following props:
    - `imageUrl`: a string representing the URL of the image to be displayed
    - `title`: a string representing the title of the card
    - `description`: a string representing the description of the card
    - `url`: a string representing the URL that the card should link to
    
    The component renders a clickable card with a background color, shadow, and image. It also displays the title and description inside the card. When clicked, the card redirects to the specified URL.

---
+++ apps/frontend/src/lib/components/Card.svelte
@@ -0,0 +1,27 @@
+<script>
+    /** @type {string} */
+    export let imageUrl;
+
+    /** @type {string} */
+    export let title;
+
+    /** @type {string}*/
+    export let description;
+
+    /** @type {string} */
+    export let url;
+</script>
+
+<a class="hover:opacity-80 duration-200" href={url}>
+  <div class="card bg-base-100 shadow-xl">
+    <figure>
+      <img src={imageUrl} alt="Shoes" />
+    </figure>
+    <div class="card-body">
+      <h2 class="card-title">
+        {title}
+      </h2>
+      <p>{description}</p>
+    </div>
+  </div>
+</a>
