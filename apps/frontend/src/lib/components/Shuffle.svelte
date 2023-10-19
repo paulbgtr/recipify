9da3e2dedcb1120f0c6a5f1d48ff11bee9cc0536
paulbg <paulbg@hey.com>
2023-10-19 08:47:19 +0300

    feat(frontend): add Shuffle component and integrate it into +page route
    - Added a new file `Shuffle.svelte` in the `lib/components` directory to create a button for shuffling recipes.
    - Modified the `+page.svelte` file in the `routes` directory to import and use the `Shuffle` component.
    - Added a section with a heading and a paragraph explaining the purpose of the shuffled recipes.
    - Integrated the `Shuffle` component into the page layout.
    - Added a sample `Card` component with dummy data for testing purposes.

---
+++ apps/frontend/src/lib/components/Shuffle.svelte
@@ -0,0 +1,1 @@
+<button class="btn btn-neutral">🎲 Shuffle</button>
--- apps/frontend/src/routes/+page.svelte
+++ apps/frontend/src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Card from "$lib/components/Card.svelte";
+    import Shuffle from "$lib/components/Shuffle.svelte";
+</script>
+
+<section class="grid gap-5">
+  <div>
+    <div class="mb-3">
+      <h2 class="font-bold text-2xl">Shuffled recipes that may interest you</h2>
+      <p class="italic font-gray-500">
+        (If you have special dietary requirements, you can set them in your <a
+          href="/settings">settings</a
+        >.)
+      </p>
+    </div>
+    <Shuffle />
+  </div>
+  <div class="grid grid-cols-3 gap-3">
+    <Card
+      title="test"
+      url="/recipes/test"
+      description="test"
+      imageUrl="https://images.immediate.co.uk/production/volatile/sites/30/2017/11/Vegetable-soup-recipes-1eb6583.jpg?resize=768,574"
+    />
+  </div>
+</section>
