89e29e90482a20e8e228daf2f41739dfc75856ef
paulbg <paulbg@hey.com>
2023-10-21 08:26:08 +0300

    feat(HealthScore.svelte): add HealthScore component to display health score with different background colors based on the score
    The HealthScore component is added to display the health score with different background colors based on the score. The component takes a `healthScore` prop, which is a number representing the health score.
    
    The `handleRateHealthScore` function is implemented to determine the background color class based on the health score. If the health score is greater than or equal to 80, the background color class will be 'success'. If the health score is between 50 and 80, the background color class will be 'warning'. Otherwise, the background color class will be 'error'.
    
    The health score is displayed inside a span element with the calculated background color class. The health score value is also displayed next to the span element.

---
+++ apps/frontend/src/lib/components/HealthScore.svelte
@@ -0,0 +1,27 @@
+<script>
+    /**
+     * @type {number}
+     */
+    export let healthScore;    
+
+    /**
+     * @param {number} healthScore
+     */
+    const handleRateHealthScore = (healthScore) => {
+        if (healthScore >= 80) {
+            return 'success';
+        } else if (healthScore >= 50) {
+            return 'warning';
+        } else {
+            return 'error';
+        }
+    }
+</script>
+
+<div class="my-2">
+  <span
+    class={`p-2 font-bold rounded-xl bg-${handleRateHealthScore(healthScore)}`}
+  >
+    Health score: {healthScore}
+  </span>
+</div>
