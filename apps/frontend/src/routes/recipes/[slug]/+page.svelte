2246ee27112f50d1bbb0433afa49607474848a59
paulbg <paulbg@hey.com>
2023-10-21 08:25:42 +0300

    feat(recipes): add new Svelte component for displaying recipe details page
    This commit adds a new Svelte component called "+page.svelte" in the "apps/frontend/src/routes/recipes/[slug]/" directory. This component is responsible for rendering the details of a recipe.
    
    The component imports the "removeHTMLTags" function from the "$lib/utils/removeHTMLTags.js" module and the "HealthScore" component from the "$lib/components/HealthScore.svelte" module.
    
    The component receives a "data" prop, which contains the necessary information for rendering the recipe details. It uses this data to display the recipe's image, title, health score, servings, ready in minutes, ingredients, and a brief summary.
    
    The "data.props.image" is used as the source for the recipe's image, and the "data.props.title" is used as the alt text.
    
    The health score, servings, and ready in minutes are displayed using the "HealthScore" component and plain text.
    
    The ingredients are rendered using a loop over the "data.props.extendedIngredients" array, displaying each ingredient's original text.
    
    Finally, the "removeHTMLTags" function is used to remove any HTML tags from the "data.props.summary" and display it as a paragraph.
    
    This new component enhances the user experience by providing a detailed view of a recipe's information.

---
+++ apps/frontend/src/routes/recipes/[slug]/+page.svelte
@@ -0,0 +1,45 @@
+<script>
+    import { removeHTMLTags } from '$lib/utils/removeHTMLTags.js';
+    import HealthScore from '$lib/components/HealthScore.svelte';
+
+    export let data;
+
+</script>
+
+<section class="grid gap-10 lg:grid-cols-2">
+  <img
+    class="w-full max-w-2xl rounded-xl"
+    src={data.props.image}
+    alt={data.props.title}
+  />
+  <div>
+    <h1 class="text-2xl font-bold">{data.props.title}</h1>
+    <div>
+      <div class="my-3">
+        <HealthScore healthScore={data.props.healthScore} />
+        <p>
+          {data.props.servings}
+          servings
+        </p>
+        <p>
+          Will be ready in
+          <span class="font-bold">
+            {data.props.readyInMinutes}
+            minutes
+          </span>
+        </p>
+      </div>
+      <h3 class="text-lg font-bold">
+        Ingredients that you need to prepare this recipe
+      </h3>
+      {#each data.props.extendedIngredients as ingredient}
+        <p>{ingredient.original}</p>
+      {/each}
+    </div>
+  </div>
+</section>
+
+<h2 class="text-xl font-bold">Brief summary</h2>
+<p>
+  {removeHTMLTags(data.props.summary)}
+</p>
