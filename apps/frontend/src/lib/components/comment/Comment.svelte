6e7459efb557302f2bb0fcc8add033805be05cd9
paulbg <paulbg@hey.com>
2023-10-23 16:24:16 +0300

    style(Comment.svelte): adjust the gap between elements in the comment component to improve spacing and alignment feat(Comment.svelte): add name, comment text, and action buttons to the comment component to enhance user experience and interaction

--- apps/frontend/src/lib/components/comment/Comment.svelte
+++ apps/frontend/src/lib/components/comment/Comment.svelte
@@ -1,14 +1,48 @@
-<div class="flex gap-4">
+<div class="flex gap-3">
   <div class="avatar">
-    <div class="w-16 h-16 rounded-full">
+    <div class="w-24 rounded-full">
       <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
     </div>
   </div>
-  <div class="flex flex-col w-full gap-3">
-    <textarea
-      placeholder="Add a comment..."
-      class="w-full textarea textarea-bordered textarea-md"
-    />
-    <button class="btn btn-primary">post</button>
+  <div>
+    <h5 class="text-xl font-bold">John Doe</h5>
+    <p class="text-gray-500">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate,
+      eaque? Facilis, facere veritatis harum nemo nisi quia laudantium sunt?
+    </p>
+    <div class="mt-3">
+      <button>Reply</button>
+      <button>Share</button>
+      <button>
+        <svg
+          class="w-6 h-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M5 15l7-7 7 7"
+          />
+        </svg>
+      </button>
+      <button>
+        <svg
+          class="w-6 h-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M19 9l-7 7-7-7"
+          />
+        </svg>
+      </button>
+    </div>
   </div>
 </div>
