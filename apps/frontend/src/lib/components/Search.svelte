fa38f4d21eafe5f4818f41a1aa0f63dbfcd0b206
paulbg <paulbg@hey.com>
2023-10-24 07:10:18 +0300

    feat(Search.svelte): add search functionality to the frontend app
    This commit adds a new component called Search.svelte to the frontend app. The component consists of a form with an input field and a search button. When the form is submitted, the onSubmit function is called. It retrieves the value from the input field and navigates to the search page using the goto function from the app/navigation module.
    
    The purpose of this change is to enable users to search for specific content within the app.

---
+++ apps/frontend/src/lib/components/Search.svelte
@@ -0,0 +1,48 @@
+<script>
+    import { goto } from '$app/navigation'
+
+    /**
+     * 
+     * @param {*} e
+     */
+    const onSubmit = e => {
+        const formData = new FormData(e.target); 
+
+        /**
+         * @type {*} 
+         */
+        const data = {}
+
+        for (let field of formData) {
+            const [key,value] = field
+            data[key] = value;
+        }
+
+        goto(`/search/${data.search}`)
+    }
+</script>
+
+<form on:submit|preventDefault={onSubmit} class="form-control">
+  <div class="join">
+    <input
+      type="text"
+      name="search"
+      placeholder="Search"
+      class="w-24 input input-bordered md:w-auto join-item"
+    />
+    <button class="btn btn-neutral join-item" type="submit">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        class="bi bi-search"
+        viewBox="0 0 16 16"
+      >
+        <path
+          d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"
+        />
+      </svg>
+    </button>
+  </div>
+</form>
