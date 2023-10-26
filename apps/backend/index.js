2ee676b6ef712639f9c74163d4161bf968e3da5d
paulbg <paulbg@hey.com>
2023-10-26 08:31:53 +0300

    Update index.js with ES6 imports

--- apps/backend/index.js
+++ apps/backend/index.js
@@ -1,11 +1,14 @@
-const express = require("express");
+import express from "express";
+import cors from "cors";
+
 const app = express();
-const port = 3000;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
 });
