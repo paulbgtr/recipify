475a855cae95a49cc20db111de58c648e338ac59
paulbg <paulbg@hey.com>
2023-10-27 15:06:36 +0300

    Implement router in the index.js file

--- apps/backend/index.js
+++ apps/backend/index.js
@@ -1,13 +1,14 @@
 import express from "express";
 import cors from "cors";
+import router from "./router.js";
+import "dotenv/config";
 
 const app = express();
 
+app.use(router);
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.get("/", router);
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
