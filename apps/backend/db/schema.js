162ac82f195a294bc100c2a1d266948fb0f2c814
paulbg <paulbg@hey.com>
2023-10-27 15:03:32 +0300

    Add schema.js with schema for users table

---
+++ apps/backend/db/schema.js
@@ -0,0 +1,10 @@
+import { pgTable, serial, text } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: serial("id").primaryKey(),
+  username: text("username").notNull(),
+  email: text("email").notNull(),
+  password: text("password").notNull(),
+  salt: text("salt").notNull(),
+  session_token: text("session_token").notNull(),
+});
