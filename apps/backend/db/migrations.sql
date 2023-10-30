19f66b36286c9297d6c4915b5a42477915e5be37
paulbg <paulbg@hey.com>
2023-10-30 16:53:04 +0200

    Change database schema
    Remove session_token and salt as I will be using JWT.

--- apps/backend/db/migrations.sql
+++ apps/backend/db/migrations.sql
@@ -4,6 +4,4 @@ create table
     username text not null,
     email text not null,
     password text not null,
-    salt text not null,
-    session_token text not null
 )
--- apps/backend/db/schema.js
+++ apps/backend/db/schema.js
@@ -5,6 +5,4 @@ export const users = pgTable("users", {
   username: text("username").notNull(),
   email: text("email").notNull(),
   password: text("password").notNull(),
-  salt: text("salt").notNull(),
-  session_token: text("session_token").notNull(),
 });
