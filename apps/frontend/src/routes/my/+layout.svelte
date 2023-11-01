2ecd0662f61323a0155e67c4a3b55394a0a2464e
paulbg <paulbg@hey.com>
2023-11-01 10:31:22 +0200

    Protect every route in /my

---
+++ apps/frontend/src/routes/my/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { goto } from "$app/navigation";
+
+    /**
+     * @type {string | null}
+     */
+    let token;
+
+    if (typeof window !== "undefined") {
+        token = localStorage.getItem("token");
+        if (!token) goto("/signin")
+    }
+</script>
+
+<slot />
