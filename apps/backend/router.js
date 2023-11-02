a2fe33ef84735bbc7075f5c6535bbd0b9e9c7297
paulbg <paulbg@hey.com>
2023-11-02 08:31:35 +0200

    Small router update and router comments
    - Separate lines of code related to different controllers.
    - Implement routes for comments controllers.

--- apps/backend/router.js
+++ apps/backend/router.js
@@ -2,14 +2,21 @@ import express from "express";
 import { getAllUsers } from "./controllers/users.js";
 import { signUp } from "./controllers/auth.js";
 import { signIn } from "./controllers/auth.js";
+import { getComments } from "./controllers/comments.js";
+import { postComment } from "./controllers/comments.js";
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.send("Hello world!");
 });
+
 router.get("/users", getAllUsers);
+
 router.post("/signup", await signUp);
 router.post("/signin", await signIn);
+
+router.get("/comments/:recipeId", await getComments);
+router.post("/comments", await postComment);
 
 export default router;
