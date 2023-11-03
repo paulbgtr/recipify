e145c5f515519674a033c595788ecaba67e2c818
paulbg <paulbg@hey.com>
2023-11-03 14:51:00 +0200

    POST comments: Additional checking of user
    Check whether a user with the username exists in the `users` table or
    not.

--- apps/backend/controllers/comments.js
+++ apps/backend/controllers/comments.js
@@ -1,5 +1,6 @@
 import { getCommentsByRecipeId } from "../db.js";
 import { createComment } from "../db.js";
+import { getUserByUsername } from "../db.js";
 
 export const getComments = async (req, res) => {
   try {
@@ -15,9 +16,16 @@ export const getComments = async (req, res) => {
 
 export const postComment = async (req, res) => {
   try {
-    const { recipe_id, user_id, rating, body } = req.body;
+    const { recipe_id, username, rating, body } = req.body;
 
-    const newComment = await createComment(recipe_id, user_id, rating, body);
+    const user = await getUserByUsername(username);
+
+    if (!user) {
+      res.status(400).json({ message: "User does not exist" });
+      return;
+    }
+
+    const newComment = await createComment(recipe_id, username, rating, body);
 
     res.status(200).json(newComment);
   } catch (err) {
